e50d70b20682b7a9243ca56b5f135aecf32ea1a9
Lillush <lillush88@gmail.com+>
2022-08-17 19:52:40 +0200

    ez könnyű volt szerencsére

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,12 +1,16 @@
+'use strict';
+
 const user = {
   firstName: 'John',
   lastName: 'Doe',
 };
 
+const { firstName, lastName, job = "unknown"} = user;
+
 //
 
-// export {
-//   firstName,
-//   lastName,
-//   job,
-// };
+export {
+   firstName,
+   lastName,
+   job,
+ };
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,4 +1,18 @@
+'use strict';
+
 const user = {
   firstName: 'John',
   lastName: 'Doe',
+};
+
+const { 
+  firstName: f, 
+  lastName: l, 
+  job: j = "unknown"
+} = user;
+
+export {
+  f,
+  l,
+  j,
 };
